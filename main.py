51f2c777b74f2c591c31abce534623a77b78552f
PedrouColares <pedro.colaressrubens15789@gmail.com>
2025-09-17 22:29:56 -0300

    main.py modified - added twoSum

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from binarySearch import BinarySearch 
+from twoSum import TwoSum
 
 if __name__ == '__main__':
     binary = BinarySearch()
     result = binary.run([1, 2, 3], 1)
-    print(result)
-    a = 5
+
+    summ = TwoSum()
+    sumResult = summ.run([1, 9, 13, 24, 12], 21)
+    print(sumResult)
